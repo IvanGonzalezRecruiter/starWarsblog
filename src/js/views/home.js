ffb84bd364207b47cdaef2ebd6d45852da3e3550
IvanGonzalezRecruiter <100708591+IvanGonzalezRecruiter@users.noreply.github.com>
2022-06-03 20:25:29 -0500

    Update home.js

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,28 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useContext } from "react";
 import "../../styles/home.css";
 import Card from "../component/Card";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<Card />
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  console.log(store);
+  //actions.verPersona(id)
+  return (
+    <div className="mt-5 container">
+      <div className="overflow-x">
+        <h1 className="text-danger">Characters</h1>
+        <div className=" d-flex">
+          <Card className="col-3" />
+          <Card className="col-3" />
+          <Card className="col-3" />
+          <Card className="col-3" />
+          <Card className="col-3" />
+          <Card className="col-3" />
+          <Card className="col-3" />
+        </div>
+      </div>
+      <h1 className="text-danger">Planets</h1>
+      <Card />
+    </div>
+  );
+};
