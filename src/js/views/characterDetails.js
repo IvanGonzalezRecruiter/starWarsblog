5f41d867e8ee0607d8e3b40aa8221ec05e50e02c
IvanGonzalezRecruiter <100708591+IvanGonzalezRecruiter@users.noreply.github.com>
2022-06-27 11:00:36 -0500

    Update characterDetails.js

--- src/js/views/characterDetails.js
+++ src/js/views/characterDetails.js
@@ -1,25 +1,49 @@
-import React from 'react'
-
-export default function CharacterDetails() {
+import React, { useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+const DPeople = () => {
+  const { store, actions } = useContext(Context);
+  const params = useParams();
+  console.log("ACTIONS");
+  useEffect(() => {
+    actions.charaInfo(params.uid);
+  }, []);
+  const index = params.uid - 1;
   return (
     <div>
-        <div className='row'>
-            <div className='col-6'>
-                <img />
-            </div>
-            <div className='col-6'>
-                <h1>aa</h1>
-                <p>aa</p>
-            </div>
+      <div className="d-flex">
+        <img height={300} width={450} src={store.imgCharacters[index].url} />
+        <div>
+          <h1 className="container text-center">{store.pjActual.name}</h1>
+        <p className="container text-center py-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce leo
+          augue, tristique id nisl ut, consequat luctus felis. Nunc egestas,
+          lacus eu pharetra eleifend, ante felis tempus mi, vel tempus felis
+          nulla facilisis lorem. In hac habitasse platea dictumst. Aenean ornare
+          semper magna sit amet volutpat. Vivamus odio enim, faucibus a turpis
+          venenatis, vestibulum dictum ex. Nulla sed porttitor ipsum, quis
+          lobortis libero. Suspendisse iaculis eleifend ante, in scelerisque
+          nibh ornare a. Vestibulum id erat augue. Aliquam vel fringilla lorem,
+          sed rutrum ex. Donec in tortor venenatis lacus cursus faucibus. Aenean
+          ut consectetur diam. Etiam rhoncus tincidunt sagittis. Duis dapibus
+          rhoncus imperdiet. Nulla quis tincidunt ipsum. Ut at leo sollicitudin,
+          mattis elit id, lobortis ex.
+        </p>
         </div>
-        <div className='row border-top border-danger' >
-            <div className='col-2'>a</div>
-            <div className='col-2'>b</div>
-            <div className='col-2'>c</div>
-            <div className='col-2'>d</div>
-            <div className='col-2'>e</div>
-            <div className='col-2'>f</div>
-        </div>
+        
+      </div>
+      
+      <div className="d-flex">
+        <p className="m-3">Height: {store.pjActual.height}</p>
+        <p className="m-3">Mass: {store.pjActual.mass}</p>
+        <p className="m-3">Skin Color: {store.pjActual.skin_color}</p>
+        <p className="m-3">Eye Color: {store.pjActual.eye_color}</p>
+        <p className="m-3">Year of birth: {store.pjActual.birth_year}</p>
+        <p className="m-3">Gender: {store.pjActual.gender}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default characterDetails;
